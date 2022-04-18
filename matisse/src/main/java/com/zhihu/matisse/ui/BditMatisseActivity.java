a72c5d6117d6e7f394ceeb69bc6ae7c6da3bd4ce
thianchai M1 <thianchai@ltman.com>
2022-04-18 21:29:27 +0700

    LTADD-3035 Redesign: redesign PhotoSelector.

---
+++ matisse/src/main/java/com/zhihu/matisse/ui/BditMatisseActivity.java
@@ -0,0 +1,335 @@
+/*
+ * Copyright 2017 Zhihu Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zhihu.matisse.ui;
+
+import static com.zhihu.matisse.ui.MatisseActivity.EXTRA_RESULT_ORIGINAL_ENABLE;
+import static com.zhihu.matisse.ui.MatisseActivity.EXTRA_RESULT_SELECTION;
+import static com.zhihu.matisse.ui.MatisseActivity.EXTRA_RESULT_SELECTION_PATH;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.database.Cursor;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.zhihu.matisse.R;
+import com.zhihu.matisse.internal.entity.Album;
+import com.zhihu.matisse.internal.entity.Item;
+import com.zhihu.matisse.internal.entity.SelectionSpec;
+import com.zhihu.matisse.internal.model.AlbumCollection;
+import com.zhihu.matisse.internal.model.SelectedItemCollection;
+import com.zhihu.matisse.internal.ui.AlbumPreviewActivity;
+import com.zhihu.matisse.internal.ui.BasePreviewActivity;
+import com.zhihu.matisse.internal.ui.BditMediaSelectionFragment;
+import com.zhihu.matisse.internal.ui.adapter.AlbumsAdapter;
+import com.zhihu.matisse.internal.ui.adapter.BditAlbumMediaAdapter;
+import com.zhihu.matisse.internal.utils.MediaStoreCompat;
+import com.zhihu.matisse.internal.utils.PathUtils;
+import com.zhihu.matisse.internal.utils.SingleMediaScanner;
+
+import java.util.ArrayList;
+
+/**
+ * Main Activity to display albums and media content (images/videos) in each album
+ * and also support media selecting operations.
+ */
+public class BditMatisseActivity extends AppCompatActivity
+        implements AlbumCollection.AlbumCallbacks,
+        AdapterView.OnItemSelectedListener,
+        BditMediaSelectionFragment.SelectionProvider,
+        BditAlbumMediaAdapter.CheckStateListener,
+        BditAlbumMediaAdapter.OnMediaClickListener,
+        BditAlbumMediaAdapter.OnPhotoCapture {
+
+    private static final int REQUEST_CODE_PREVIEW = 23;
+    private static final int REQUEST_CODE_CAPTURE = 24;
+    public static final String CHECK_STATE = "checkState";
+    private final AlbumCollection mAlbumCollection = new AlbumCollection();
+    private MediaStoreCompat mMediaStoreCompat;
+    private SelectedItemCollection mSelectedCollection = new SelectedItemCollection(this);
+    private SelectionSpec mSpec;
+
+    private AlbumsAdapter mAlbumsAdapter;
+    private View mContainer;
+    private View mEmptyView;
+
+    private boolean mOriginalEnable;
+
+    private MenuItem optionMenu = null;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        // programmatically set theme before super.onCreate()
+        mSpec = SelectionSpec.getInstance();
+        setTheme(mSpec.themeId);
+        super.onCreate(savedInstanceState);
+        if (!mSpec.hasInited) {
+            setResult(RESULT_CANCELED);
+            finish();
+            return;
+        }
+        setContentView(R.layout.activity_bdit_matisse);
+
+        if (mSpec.needOrientationRestriction()) {
+            setRequestedOrientation(mSpec.orientation);
+        }
+
+        if (mSpec.capture) {
+            mMediaStoreCompat = new MediaStoreCompat(this);
+            if (mSpec.captureStrategy == null)
+                throw new RuntimeException("Don't forget to set CaptureStrategy.");
+            mMediaStoreCompat.setCaptureStrategy(mSpec.captureStrategy);
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(ContextCompat.getDrawable(this, R.drawable.ic_cancel_nav));
+        Drawable navigationIcon = toolbar.getNavigationIcon();
+        TypedArray ta = getTheme().obtainStyledAttributes(new int[]{R.attr.album_element_color});
+        int color = ta.getColor(0, 0);
+        ta.recycle();
+        navigationIcon.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+
+        mContainer = findViewById(R.id.container);
+        mEmptyView = findViewById(R.id.empty_view);
+
+        mSelectedCollection.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            mOriginalEnable = savedInstanceState.getBoolean(CHECK_STATE);
+        }
+
+        mAlbumsAdapter = new AlbumsAdapter(this, null, false);
+        mAlbumCollection.onCreate(this, this);
+        mAlbumCollection.onRestoreInstanceState(savedInstanceState);
+        mAlbumCollection.loadAlbums();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mSelectedCollection.onSaveInstanceState(outState);
+        mAlbumCollection.onSaveInstanceState(outState);
+        outState.putBoolean("checkState", mOriginalEnable);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAlbumCollection.onDestroy();
+        mSpec.onCheckedListener = null;
+        mSpec.onSelectedListener = null;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        if (item.getItemId() == R.id.action_next) {
+            Intent result = new Intent();
+            ArrayList<Uri> selectedUris = (ArrayList<Uri>) mSelectedCollection.asListOfUri();
+            result.putParcelableArrayListExtra(EXTRA_RESULT_SELECTION, selectedUris);
+            ArrayList<String> selectedPaths = (ArrayList<String>) mSelectedCollection.asListOfString();
+            result.putStringArrayListExtra(EXTRA_RESULT_SELECTION_PATH, selectedPaths);
+            result.putExtra(EXTRA_RESULT_ORIGINAL_ENABLE, mOriginalEnable);
+            setResult(RESULT_OK, result);
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        setResult(Activity.RESULT_CANCELED);
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK)
+            return;
+
+        if (requestCode == REQUEST_CODE_PREVIEW) {
+            Bundle resultBundle = data.getBundleExtra(BasePreviewActivity.EXTRA_RESULT_BUNDLE);
+            ArrayList<Item> selected = resultBundle.getParcelableArrayList(SelectedItemCollection.STATE_SELECTION);
+            mOriginalEnable = data.getBooleanExtra(BasePreviewActivity.EXTRA_RESULT_ORIGINAL_ENABLE, false);
+            int collectionType = resultBundle.getInt(SelectedItemCollection.STATE_COLLECTION_TYPE,
+                    SelectedItemCollection.COLLECTION_UNDEFINED);
+            if (data.getBooleanExtra(BasePreviewActivity.EXTRA_RESULT_APPLY, false)) {
+                Intent result = new Intent();
+                ArrayList<Uri> selectedUris = new ArrayList<>();
+                ArrayList<String> selectedPaths = new ArrayList<>();
+                if (selected != null) {
+                    for (Item item : selected) {
+                        selectedUris.add(item.getContentUri());
+                        selectedPaths.add(PathUtils.getPath(this, item.getContentUri()));
+                    }
+                }
+                result.putParcelableArrayListExtra(EXTRA_RESULT_SELECTION, selectedUris);
+                result.putStringArrayListExtra(EXTRA_RESULT_SELECTION_PATH, selectedPaths);
+                result.putExtra(EXTRA_RESULT_ORIGINAL_ENABLE, mOriginalEnable);
+                setResult(RESULT_OK, result);
+                finish();
+            } else {
+                mSelectedCollection.overwrite(selected, collectionType);
+                Fragment bditMediaSelectionFragment = getSupportFragmentManager().findFragmentByTag(
+                        BditMediaSelectionFragment.class.getSimpleName());
+                if (bditMediaSelectionFragment instanceof BditMediaSelectionFragment) {
+                    ((BditMediaSelectionFragment) bditMediaSelectionFragment).refreshMediaGrid();
+                }
+            }
+        } else if (requestCode == REQUEST_CODE_CAPTURE) {
+            // Just pass the data back to previous calling Activity.
+            Uri contentUri = mMediaStoreCompat.getCurrentPhotoUri();
+            String path = mMediaStoreCompat.getCurrentPhotoPath();
+            ArrayList<Uri> selected = new ArrayList<>();
+            selected.add(contentUri);
+            ArrayList<String> selectedPath = new ArrayList<>();
+            selectedPath.add(path);
+            Intent result = new Intent();
+            result.putParcelableArrayListExtra(EXTRA_RESULT_SELECTION, selected);
+            result.putStringArrayListExtra(EXTRA_RESULT_SELECTION_PATH, selectedPath);
+            setResult(RESULT_OK, result);
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+                BditMatisseActivity.this.revokeUriPermission(contentUri,
+                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+            new SingleMediaScanner(this.getApplicationContext(), path, () -> Log.i("SingleMediaScanner", "scan finish!"));
+            finish();
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        mAlbumCollection.setStateCurrentSelection(position);
+        mAlbumsAdapter.getCursor().moveToPosition(position);
+        Album album = Album.valueOf(mAlbumsAdapter.getCursor());
+        if (album.isAll() && SelectionSpec.getInstance().capture) {
+            album.addCaptureCount();
+        }
+        onAlbumSelected(album);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        //Do nothing
+    }
+
+    @Override
+    public void onAlbumLoad(final Cursor cursor) {
+        mAlbumsAdapter.swapCursor(cursor);
+        // select default album.
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(() -> {
+            cursor.moveToPosition(mAlbumCollection.getCurrentSelection());
+            Album album = Album.valueOf(cursor);
+            if (album.isAll() && SelectionSpec.getInstance().capture) {
+                album.addCaptureCount();
+            }
+            onAlbumSelected(album);
+        });
+    }
+
+    @Override
+    public void onAlbumReset() {
+        mAlbumsAdapter.swapCursor(null);
+    }
+
+    private void onAlbumSelected(Album album) {
+        if (album.isAll() && album.isEmpty()) {
+            mContainer.setVisibility(View.GONE);
+            mEmptyView.setVisibility(View.VISIBLE);
+        } else {
+            mContainer.setVisibility(View.VISIBLE);
+            mEmptyView.setVisibility(View.GONE);
+            Fragment fragment = BditMediaSelectionFragment.newInstance(album);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, fragment, BditMediaSelectionFragment.class.getSimpleName())
+                    .commitAllowingStateLoss();
+        }
+    }
+
+    @Override
+    public void onUpdate() {
+        this.optionMenu.setEnabled(mSelectedCollection.count() > 0);
+
+        if (mSpec.onSelectedListener != null) {
+            mSpec.onSelectedListener.onSelected(
+                    mSelectedCollection.asListOfUri(), mSelectedCollection.asListOfString());
+        }
+    }
+
+    @Override
+    public void onMediaClick(Album album, Item item, int adapterPosition) {
+        Intent intent = new Intent(this, AlbumPreviewActivity.class);
+        intent.putExtra(AlbumPreviewActivity.EXTRA_ALBUM, album);
+        intent.putExtra(AlbumPreviewActivity.EXTRA_ITEM, item);
+        intent.putExtra(BasePreviewActivity.EXTRA_DEFAULT_BUNDLE, mSelectedCollection.getDataWithBundle());
+        intent.putExtra(BasePreviewActivity.EXTRA_RESULT_ORIGINAL_ENABLE, mOriginalEnable);
+        startActivityForResult(intent, REQUEST_CODE_PREVIEW);
+    }
+
+    @Override
+    public SelectedItemCollection provideSelectedItemCollection() {
+        return mSelectedCollection;
+    }
+
+    @Override
+    public void capture() {
+        if (mMediaStoreCompat != null) {
+            mMediaStoreCompat.dispatchCaptureIntent(this, REQUEST_CODE_CAPTURE);
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_next, menu);
+        this.optionMenu = menu.findItem(R.id.action_next);
+        this.optionMenu.setTitle("Next");
+        this.optionMenu.setEnabled(false);
+        return true;
+    }
+
+}
